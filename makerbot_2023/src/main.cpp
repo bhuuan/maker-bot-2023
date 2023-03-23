48bc102eba49a28c071036f9ab9b1a3ce6d294d6
ltvinh <everwellmax@gmail.com>
2023-03-23 18:15:43 +0700

    modified src/main.cpp (added signal LED)

--- makerbot_2023/src/main.cpp
+++ makerbot_2023/src/main.cpp
@@ -1,6 +1,6 @@
 #include <BluetoothSerial.h>
 BluetoothSerial btSerial;
-#define BT_NAME "ESP32BT-SCT" // Set bluetooth name
+#define BT_NAME "ESP32BT-SCT-Test" // Set bluetooth name
 
 // Include Wire Library for I2C
 #include <Wire.h>
@@ -30,7 +30,7 @@ void goForward(int leftSpeed, int rightSpeed)
   pca9685.setPWM(12, 4096, 0);
   pca9685.setPWM(13, 0, leftSpeed);
 
-  pca9685.setPWM(8, 0, rightSpeed);
+  pca9685.setPWM(8, 0, rightSpeed);-
   pca9685.setPWM(9, 4096, 0);
   pca9685.setPWM(10, 0, rightSpeed);
   pca9685.setPWM(11, 4096, 0);
@@ -225,9 +225,13 @@ void bluetoothControl()
       digitalWrite(32, 1);
     break;
 
-    case 'n':
+    case 'f':
       digitalWrite(2, 0);
       digitalWrite(32, 0);
+    break;
+    case 'n':
+      digitalWrite(2, 1);
+      digitalWrite(32, 1);
     break;
   }
 }
